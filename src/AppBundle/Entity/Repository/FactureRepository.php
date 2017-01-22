05d9f52cde378ef49784a597f0e580de4acd8024
Ulas ATILA <ulas.atila@gmail.com>
2017-01-22 20:31:25 +0100

    Edit user factures date

--- src/AppBundle/Entity/Repository/FactureRepository.php
+++ src/AppBundle/Entity/Repository/FactureRepository.php
@@ -5,6 +5,20 @@ use Doctrine\ORM\EntityRepository;
 
 class FactureRepository extends EntityRepository
 {
+    public function findForUser($profil, $dateDebut, $dateFin)
+    {
+        $qb = $this->createQueryBuilder('f');
+        $qb
+            ->where('f.profil = ?1')
+            ->andWhere('f.etat = true')
+            ->andWhere('f.date BETWEEN ?2 AND ?3')
+            ->setParameter(1, $profil)
+            ->setParameter(2, $dateDebut)
+            ->setParameter(3, $dateFin);
+
+        return $qb->getQuery()->execute();
+    }
+
     public function countNonValide()
     {
         $qb = $this->getEntityManager()->createQueryBuilder();
